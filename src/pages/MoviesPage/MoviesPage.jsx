257c195530a2d1bbe0a16c1555ad2561cf76693e
sno0wi <sno0wi20@gmail.com>
2024-03-20 15:05:23 +0200

    done moviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { requestSearchMovie } from "../../services/api.js";
 import { Formik, Field, Form } from "formik";
 import Loader from "../../components/Loader/Loader.jsx";
@@ -10,15 +11,18 @@ const MoviesPage = () => {
   const [searchFilm, setSearchFilm] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getData = async () => {
       try {
         setIsLoading(true);
-        const data = await requestSearchMovie(searchFilm);
+        const data = await requestSearchMovie(searchFilm.search);
         setFilms(data);
       } catch {
-        setIsError(true);
+        if (searchFilm !== null) {
+          setIsError(true);
+        }
       } finally {
         setIsLoading(false);
       }
@@ -30,18 +34,15 @@ const MoviesPage = () => {
   const handleSubmit = (data, formActions) => {
     setSearchFilm(data);
     formActions.resetForm();
+    navigate(`/movies?query=${data.search}`);
   };
 
   const INITIAL_FORM_DATA = {
     search: "",
   };
 
-  console.log(films);
-
   return (
     <>
-      {isError && <ErrorMessage />}
-      {isLoading && <Loader />}
       <Formik initialValues={INITIAL_FORM_DATA} onSubmit={handleSubmit}>
         <Form>
           <Field type="text" name="search" />
@@ -50,13 +51,13 @@ const MoviesPage = () => {
           </button>
         </Form>
       </Formik>
-      {films && (
+      {isError && <ErrorMessage />}
+      {isLoading && <Loader />}
+      {films && searchFilm !== null && (
         <ul>
           {films.results.map((film) => (
             <li key={film.id}>
-              <Link to={`/movies?query=${searchFilm}`}>
-                {film.original_title}
-              </Link>
+              <Link to={`/movies/${film.id}`}>{film.original_title}</Link>
             </li>
           ))}
         </ul>
