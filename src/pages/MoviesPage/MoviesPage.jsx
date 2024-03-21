519fb7cf32f59e22cd9b4576c7a1f4f0e3d64859
sno0wi <sno0wi20@gmail.com>
2024-03-21 06:42:00 +0200

    MovPage add useSearchParams

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,51 +1,56 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { requestSearchMovie } from "../../services/api.js";
 import { Formik, Field, Form } from "formik";
 import Loader from "../../components/Loader/Loader.jsx";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage.jsx";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const MoviesPage = () => {
   const [films, setFilms] = useState(null);
-  const [searchFilm, setSearchFilm] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchQuery = searchParams.get("query");
 
   useEffect(() => {
+    if (searchQuery === null) {
+      return;
+    }
+
     const getData = async () => {
       try {
         setIsLoading(true);
-        const data = await requestSearchMovie(searchFilm.search);
+        const data = await requestSearchMovie(searchQuery);
         setFilms(data);
       } catch {
-        if (searchFilm !== null) {
-          setIsError(true);
-        }
+        setIsError(true);
       } finally {
         setIsLoading(false);
       }
     };
 
     getData();
-  }, [searchFilm]);
+  }, [searchQuery]);
 
-  const handleSubmit = (data, formActions) => {
-    setSearchFilm(data);
+  const handleSubmit = (searchTerm, formActions) => {
+    if (searchTerm.trim().length === 0) {
+      alert("Please enter a search term first!");
+      return;
+    }
+    setSearchParams({ query: searchTerm });
     formActions.resetForm();
-    navigate(`/movies?query=${data.search}`);
-  };
-
-  const INITIAL_FORM_DATA = {
-    search: "",
   };
 
   return (
     <>
-      <Formik initialValues={INITIAL_FORM_DATA} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={{ query: searchQuery ?? "" }}
+        onSubmit={(values) => {
+          handleSubmit(values.query);
+        }}
+      >
         <Form>
-          <Field type="text" name="search" />
+          <Field placeholder="Search" type="text" name="query" />
           <button type="submit" title="Click to search" aria-label="Search">
             Search
           </button>
@@ -53,7 +58,7 @@ const MoviesPage = () => {
       </Formik>
       {isError && <ErrorMessage />}
       {isLoading && <Loader />}
-      {films && searchFilm !== null && (
+      {films && (
         <ul>
           {films.results.map((film) => (
             <li key={film.id}>
